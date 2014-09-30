b45dda3c38b39a80427f8a279addea5250ac3743
robert-fiedor <robert.fiedor@kaplan.com>
2014-09-29 22:37:24 -0400

    2 tests passing

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 993TDDnode
 ==========
+
+http://code.tutsplus.com/tutorials/testing-in-nodejs--net-35018
+
+	
+npm install -g mocha
+
+When that completes create a new folder for our project and run the following inside it:
+1
+	
+npm install chai
+
+a pozniej tylko 
+
+
+mocha
+mocha -w 
+
+(watch)
---
+++ test/tagsSpec.js
@@ -0,0 +1,21 @@
+
+//http://code.tutsplus.com/tutorials/testing-in-nodejs--net-35018
+
+var expect = require("chai").expect;
+var tags = require("../lib/tags.js");
+ 
+describe("Tags", function(){
+   describe("#parse()", function(){
+
+       it("ova", function(){
+         
+         expect(4+5).equal(9);
+       });
+
+       it("ova lalalaql", function(){
+
+           expect(4+1).equal(5);
+       });
+
+   });
+});
